c82844e7dffb1815d2ff6cfe5302fa14a336defa
rewook <j.piekarek@gmail.com>
2023-03-12 14:29:08 +0100

    Gestion des régimes

---
+++ src/Controller/BACKOFFICE/RegimeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller\BACKOFFICE;
+
+use App\Entity\Regime;
+use App\Form\RegimeType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegimeController extends AbstractController
+{
+    #[Route('/admin/regime', name: 'show_admin_regime')]
+    public function show(EntityManagerInterface $em): Response
+    {
+        $regimes = $em->getRepository(Regime::class)->findAll();
+
+
+        return $this->render('backoffice/regime/show.html.twig', [
+            'regimes' => $regimes,
+        ]);
+    }
+
+    #[Route('/admin/regime/add', name: 'add_admin_regime')]
+    public function add(Request $request,EntityManagerInterface $em): Response
+    {
+        $regime = new Regime();
+        $form = $this->createForm(RegimeType::class, $regime);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($regime);
+            $em->flush();
+            $this->addFlash('success', 'Régime ajouté avec succès');
+            return $this->redirectToRoute('show_admin_regime');
+        }
+
+        return $this->render('BACKOFFICE/regime/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/regime/{id}', name: 'edit_admin_regime')]
+    public function edit(Request $request,EntityManagerInterface $em,$id): Response
+    {
+        $regime = $em->getRepository(Regime::class)->find($id);
+
+        $form = $this->createForm(RegimeType::class, $regime);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($regime);
+            $em->flush();
+            $this->addFlash('success', 'Régime modifié avec succès');
+            return $this->redirectToRoute('show_admin_regime');
+        }
+
+        return $this->render('BACKOFFICE/regime/edit.html.twig', [
+            'form' => $form->createView(),
+            'regime' => $regime,
+        ]);
+
+
+    }
+
+    #[Route('/admin/regime/delete/{id}', name: 'delete_admin_regime')]
+    public function delete(Request $request,EntityManagerInterface $em,$id): Response
+    {
+
+        $regime = $em->getRepository(Regime::class)->find($id);
+
+
+
+        $em->remove($regime);
+        $em->flush();
+
+        $this->addFlash('success', 'Régime supprimé avec succès');
+
+        return $this->redirectToRoute('show_admin_regime');
+
+
+    }
+}
---
+++ src/Form/RegimeType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Regime;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegimeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom',TextType::class,[
+                'label' => 'Nom'
+            ])
+            ->add('description',TextareaType::class,[
+                'label' => 'Description'
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Regime::class,
+        ]);
+    }
+}

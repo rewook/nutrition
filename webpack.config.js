bee5bb256a863fe2428b4ca44c44c4d2365a01d2
rewook <j.piekarek@gmail.com>
2023-03-15 23:39:45 +0100

    Mis en place d'un jumbotron

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,14 @@ Encore
         config.corejs = '3.23';
     })
 
+    .copyFiles({
+        from: './assets/images',
 
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]',
+
+
+    })
 
 
     // enables Sass/SCSS support
